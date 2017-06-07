048ac92a89e32b39d95b44ef11a29670aeae3c72
Asia Kane and Kat Gifford <kat@>
2017-06-07 14:40:38 -0700

    add pipe and function to filter pipe

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,8 +4,20 @@ import { Task }       from './task.model';
 @Component({
   selector: 'task-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="allTasks">All Tasks</option>
+    <option value="completedTasks">Completed Tasks</option>
+    <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+  </select>
+
   <ul>
-    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
+    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}}
+
+    <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+
+    <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+
+    <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
   </ul>
   `
 })
@@ -13,6 +25,7 @@ import { Task }       from './task.model';
 export class TaskListComponent {
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
+  filterByCompleteness: string = "incompleteTasks";
 
   editButtonHasBeenClicked(taskToEdit: Task) {
     this.clickSender.emit(taskToEdit);
@@ -26,13 +39,22 @@ export class TaskListComponent {
     }
   }
 
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
   }
+
+  toggleDone() {
+
+  }
+
+
+  // priorityColor(currentTask){
+  //   if (currentTask.priority === 3){
+  //     return "bg-danger";
+  //   } else if (currentTask.priority === 2) {
+  //     return  "bg-warning";
+  //   } else {
+  //     return "bg-info";
+  //   }
+  // }
 }
